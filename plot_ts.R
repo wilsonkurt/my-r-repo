61565563beb87a555887b2eed62c1c757d072bfb
Kurt Wilson <wilson.kurtm@gmail.com>
2021-01-13 14:56:37 -0700

    plotted co2 for practice with R and git

--- plot_ts.R
+++ plot_ts.R
@@ -3,3 +3,10 @@ plot_ts <- function(x,y,...) {
   plot(x, y, type = 'l', ...)
   lines(x, fitted(fit), col = 2, lwd = 2)
 }
+
+#load function
+source("plot_ts.R")
+#load file
+co2 <- read.csv("co2_mm_mlo.csv")
+# make plot
+plot_ts(co2$decdate, co2$interpolated, main = "Mauna Loa CO2 Record")
